eaf6ba1f7dd2c6f078fff2e5c2cf1ce6c30bd170
kkryus <kamilkryus@gmail.com>
2018-05-13 14:53:47 +0200

    now we are able to add sudoku

---
+++ addSudokuScripts.js
@@ -0,0 +1,44 @@
+var currentLevel = "easy";
+
+function undisableLevel(id)
+{
+	document.getElementById(currentLevel).classList.add("notUsed");
+	document.getElementById(id).classList.remove("notUsed");
+	currentLevel = id;
+}
+
+function saveToFile()
+{
+	var fulfilled = document.getElementById("fulfilled").value;
+	var showed = document.getElementById("showed").value;
+	$.ajax({
+        url: "phpScripts/subPlayScripts.php",
+		data: { 
+        'level': currentLevel
+		},
+        type: 'GET',
+        cache: false,
+        timeout: 30000,
+		async: false,
+        error: function(){
+            return true;
+        },
+        success: function(data){ 
+            sudokus = data;
+        }
+    });
+	sudokus = sudokus.split(",");
+	var fileName = sudokus.length - 2 + 1;
+	
+	
+	$.post("phpScripts/saveSudoku.php", {
+		fulfilled:fulfilled,
+		showed:showed,
+		gameFolder:currentLevel,
+		fileName:fileName
+		}, function(data) {
+			if (data != "") {
+				console.log(data);
+			}
+		});	
+}
